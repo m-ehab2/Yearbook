0713a70e43b599e79ada1dcb0c7931cfdff3b71c
m-ehab2 <muhamedehab1@gmail.com>
2024-03-06 18:05:03 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default App;
