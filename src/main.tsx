b9016e91612cd75ee948c6440625570893dc1ae7
m-ehab2 <muhamedehab1@gmail.com>
2024-03-10 00:53:13 +0200

    added firebase initialization and removed routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginPage from "./pages/Login/LoginPage.tsx";
-import SignUp from "./pages/SignUp/SignUp.tsx";
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebaseConfig";
-
+import { firebaseConfig } from "./firebaseConfig.ts";
 initializeApp(firebaseConfig);
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  { path: "/login", element: <LoginPage /> },
-  { path: "/register", element: <SignUp /> },
-]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
