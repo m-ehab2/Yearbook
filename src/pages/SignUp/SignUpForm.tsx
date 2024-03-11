a83b46a8739eeda63bf160b6f96454f1bcdc4312
m-ehab2 <muhamedehab1@gmail.com>
2024-03-11 23:03:31 +0200

    handled a new pattern for the password

--- src/pages/SignUp/SignUpForm.tsx
+++ src/pages/SignUp/SignUpForm.tsx
@@ -47,7 +47,7 @@ export default function SignUpForm() {
     password: Yup.string()
       .required("Required")
       .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&-])[A-Za-z\d@$!%*?&-]{8,}$/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&-])[A-Za-z\d@$!#%*?&-]{8,}$/,
         "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character"
       ),
     batch: Yup.string().matches(/(MEARN2024)/, "Select A Batch"),
