d828311791e662f5a97399ce3c3550dbb931f22e
m-ehab2 <muhamedehab1@gmail.com>
2024-03-11 23:02:57 +0200

    added toasts  for the signup process

--- src/pages/SignUp/SignUpForm.tsx
+++ src/pages/SignUp/SignUpForm.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
+  Checkbox,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  Link,
   MenuItem,
+  OutlinedInput,
   Select,
   TextField,
   Typography,
@@ -10,24 +17,25 @@ import {
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { firebaseConfig } from "../../firebaseConfig";
-import { initializeApp } from "firebase/app";
 import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-initializeApp(firebaseConfig);
-
-const auth = getAuth();
-const db = getFirestore();
-
-const SignUpForm: React.FC = () => {
+export default function SignUpForm() {
+  const auth = getAuth();
+  const db = getFirestore();
   const initialValues = {
     firstName: "",
     lastName: "",
     email: "",
     password: "",
     batch: "s",
+    agreedToTerms: false,
   };
-
+  const navigate = useNavigate();
   const validationSchema = Yup.object().shape({
     firstName: Yup.string()
       .min(3, `Must be more than 2 letters`)
@@ -43,23 +51,43 @@ const SignUpForm: React.FC = () => {
         "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character"
       ),
     batch: Yup.string().matches(/(MEARN2024)/, "Select A Batch"),
+    agreedToTerms: Yup.boolean().oneOf(
+      [true],
+      "You must agree to the terms and conditions"
+    ),
   });
 
   const handleSubmit = async (values: typeof initialValues) => {
-    console.log("Form submitted with values:", values);
     try {
       const { email, password, firstName, lastName, batch } = values;
-      const user = await createUserWithEmailAndPassword(auth, email, password);
-      console.log("User signed up successfully!", user);
-      const docRef = await addDoc(collection(db, "users"), {
+      const user = await toast.promise(
+        createUserWithEmailAndPassword(auth, email, password),
+        {
+          pending: "Registration is pending",
+          success: "Registration Successful",
+          error: "Registration Failed",
+        }
+      );
+      await addDoc(collection(db, "users"), {
         firstName,
         lastName,
         batch,
         email,
+        uid: user.user.uid,
+        xLink: "",
+        linkedInLink: "",
+        gitHubLink: "",
+        motto: "",
+        title: "",
       });
-      console.log("Document written with ID: ", docRef.id);
+      navigate("/myprofile");
     } catch (error: unknown) {
-      console.error("Error signing up:", error.message);
+      const errorMessage = (error as Error).message;
+      if (errorMessage.includes("auth/email-already-in-use")) {
+        toast.error("Email Already Registered");
+      } else {
+        toast.error("An unexpected error occurred. Please try again later.");
+      }
     }
   };
 
@@ -68,7 +96,15 @@ const SignUpForm: React.FC = () => {
     validationSchema,
     onSubmit: handleSubmit,
   });
+  const [showPassword, setShowPassword] = useState(false);
 
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
   return (
     <Box
       component="form"
@@ -120,8 +156,8 @@ const SignUpForm: React.FC = () => {
       <Box>
         <TextField
           label="Email"
-          fullWidth
           margin="normal"
+          fullWidth
           {...formik.getFieldProps("email")}
           error={formik.touched.email && Boolean(formik.errors.email)}
         />
@@ -129,16 +165,32 @@ const SignUpForm: React.FC = () => {
           {formik.touched.email && formik.errors.email}
         </Typography>
       </Box>
-      <Box sx={{ marginBottom: "10px" }}>
-        <TextField
-          label="Password"
-          type="password"
-          fullWidth
-          margin="normal"
-          {...formik.getFieldProps("password")}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-        />
-        <Typography sx={{ height: "32px" }} variant="body2" color="error">
+      <Box>
+        <FormControl margin="normal" sx={{ width: "100%" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            {...formik.getFieldProps("password")}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <Typography sx={{ height: "48px" }} variant="body2" color="error">
           {formik.touched.password && formik.errors.password}
         </Typography>
       </Box>
@@ -152,14 +204,39 @@ const SignUpForm: React.FC = () => {
         <MenuItem value="s">Select A Batch</MenuItem>
         <MenuItem value="MEARN2024">Mearn 2024</MenuItem>
       </Select>
-      <Typography sx={{ height: "32px" }} variant="body2" color="error">
+      <Typography sx={{ height: "16px" }} variant="body2" color="error">
         {formik.touched.batch && formik.errors.batch}
       </Typography>
-      <Button type="submit" variant="contained" color="primary" fullWidth>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Checkbox {...formik.getFieldProps("agreedToTerms")} color="primary" />
+        <Typography variant="body2">
+          I agree to the <a href="/terms">terms and conditions</a>
+        </Typography>
+      </Box>
+      <Typography sx={{ height: "32px" }} variant="body2" color="error">
+        {formik.touched.agreedToTerms && formik.errors.agreedToTerms}
+      </Typography>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        sx={{ marginBottom: "10px" }}
+      >
         Sign Up
       </Button>
+      <Typography>
+        Already Have an account?
+        <Link
+          component={RouterLink}
+          to={"/login"}
+          color="primary"
+          underline="none"
+          sx={{ marginLeft: 1, fontFamily: "Roboto" }}
+        >
+          Login
+        </Link>
+      </Typography>
     </Box>
   );
-};
-
-export default SignUpForm;
+}
